33784893243c4aae512b23443f451e477e64db75
Renzo Pimentel Uzal <renzopimentel@gmail.com>
2022-09-27 16:11:52 -0300

    BaSP-W06-2022: Login changes and Sign-Up JS done

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,39 +1,40 @@
 window.onload = function() {
 
-    //Variables
-    var formEmail = document.getElementById('email-form');
-    var formPassword = document.getElementById('password-form');
-    var passwordInput = document.getElementById('password-input');
-    var emailInput = document.getElementById('email-input');
-    var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
     var p = document.createElement('p');
     var button = document.getElementById('btn-login');
 
    /* EMAIL VALIDATION */
+   var formEmail = document.getElementById('email-form');
+   var emailInput = document.getElementById('email-input');
+   var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+
     formEmail.onblur = function() {
         if (formEmail.value === '') {
-            formEmail.classList.add('border-red');
+            formEmail.classList.add('error');
             p.innerHTML = 'Email is required'
             emailInput.appendChild(p);
         } else if (!emailExpression.test(formEmail.value)) {
-            formEmail.classList.add('border-red');
+            formEmail.classList.add('error');
             p.innerHTML = 'Correct format should be email@domain.com';
             emailInput.appendChild(p);
         } else {
-            formEmail.classList.add('border-green');
+            formEmail.classList.add('valid');
         }
     }
 
     formEmail.onfocus = function () {
-        formEmail.classList.remove('border-red');
+        formEmail.classList.remove('error');
         emailInput.removeChild(p);
     }
 
     /* PASSWORD VALIDATION */
+    var formPassword = document.getElementById('password-form');
+    var passwordInput = document.getElementById('password-input');
+
     formPassword.onblur = function() {
         var numbers = "0123456789";
-        var validationNumber = false
         var letters = "abcdefghyjklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+        var validationNumber = false
         var validationLetters = false
 
         for(i=0; i<formPassword.value.length; i++){
@@ -46,26 +47,26 @@ window.onload = function() {
         }
 
         if (formPassword.value === '') {
-            formPassword.classList.add('border-red');
+            formPassword.classList.add('error');
             p.innerHTML = 'Password is required';
             passwordInput.appendChild(p);
         } else if (formPassword.value.length < 8 || validationNumber != true || validationLetters != true) {
-            formPassword.classList.add('border-red');
+            formPassword.classList.add('error');
             p.innerHTML = 'Password must have at least 8 characters of letters and numbers';
             passwordInput.appendChild(p);
         } else {
-            formPassword.classList.add('border-green');
+            formPassword.classList.add('valid');
         }
     }
 
     formPassword.onfocus = function () {
-        formPassword.classList.remove('border-red');
+        formPassword.classList.remove('error');
         passwordInput.removeChild(p);
     }
 
     /* BUTTON */
     button.onclick = function(){
-        if (formEmail.classList.contains('border-green') && formPassword.classList.contains('border-green')){
+        if (formEmail.classList.contains('valid') && formPassword.classList.contains('valid')){
             alert('Mail: ' + formEmail.value + '\nPassword: ' + formPassword.value);
         } else {
             alert('Login Error');
--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,11 +1,42 @@
 window.onload = function(){
+    /* FUNCTIONS */
+    function hasLetters(input) {
+        for (var i = 0; i < input.length; i++){
+            var charCode = input.charCodeAt(i)
+            if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    function hasNumbers(input) {
+        for (var i = 0; i < input.length; i++){
+            var charCode = input.charCodeAt(i)
+            if ((charCode > 47 && charCode < 58)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    function removeP(input) {
+        input.classList.remove('error');
+        if (input.nextElementSibling) {
+            input.parentElement.removeChild(input.nextElementSibling);
+        }
+    }
+
     var inputs = document.getElementsByTagName('input');
     var inputsP = document.getElementsByClassName('form-input');
-    var p = document.createElement('p');
+    var numbers = "0123456789";
+    var letters = "abcdefghyjklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
     /* NAME VALIDATION */
     var formName = inputs[0];
-    var nameP = inputsP[0];
+    var nameP = document.createElement('p');
 
     formName.onblur = function() {
         var letters;
@@ -21,30 +52,29 @@ window.onload = function(){
         }
 
         if (formName.value == '') {
-            formName.classList.add('border-red')
-            p.innerHTML = 'Name is required'
-            nameP.appendChild(p);
+            formName.classList.add('error')
+            nameP.innerHTML = 'Name is required'
+            formName.parentElement.insertBefore(nameP, formName.nextElementSibling)
         } else if (formName.value.length < 4) {
-            formName.classList.add('border-red')
-            p.innerHTML = 'Name should have more than 3 letters'
-            nameP.appendChild(p);
+            formName.classList.add('error')
+            nameP.innerHTML = 'Name should have more than 3 letters'
+            formName.parentElement.insertBefore(nameP, formName.nextElementSibling)
         } else if (!letters) {
-            formName.classList.add('border-red')
-            p.innerHTML = 'Name should have only letters'
-            nameP.appendChild(p);
+            formName.classList.add('error')
+            nameP.innerHTML = 'Name should have only letters'
+            formName.parentElement.insertBefore(nameP, formName.nextElementSibling)
         } else {
-            formName.classList.add('border-green')
+            formName.classList.add('valid')
         }
     }
 
     formName.onfocus = function () {
-        formName.classList.remove('border-red');
-        nameP.removeChild(p);
+        removeP(formName);
     }
 
     /* LAST NAME VALIDATION */
     var formLastName = inputs[1];
-    var lastNameP = inputsP[1];
+    var lastNameP = document.createElement('p');
 
     formLastName.onblur = function() {
         var letters;
@@ -60,30 +90,29 @@ window.onload = function(){
         }
 
         if (formLastName.value == '') {
-            formLastName.classList.add('border-red')
-            p.innerHTML = 'Last name is required'
-            lastNameP.appendChild(p);
+            formLastName.classList.add('error')
+            lastNameP.innerHTML = 'Last name is required'
+            formLastName.parentElement.insertBefore(lastNameP, formLastName.nextElementSibling)
         } else if (formLastName.value.length < 4) {
-            formLastName.classList.add('border-red')
-            p.innerHTML = 'Last name should have more than 3 letters'
-            lastNameP.appendChild(p);
+            formLastName.classList.add('error')
+            lastNameP.innerHTML = 'Last name should have more than 3 letters'
+            formLastName.parentElement.insertBefore(lastNameP, formLastName.nextElementSibling)
         } else if (!letters) {
-            formLastName.classList.add('border-red')
-            p.innerHTML = 'Last name should have only letters'
-            lastNameP.appendChild(p);
+            formLastName.classList.add('error')
+            lastNameP.innerHTML = 'Last name should have only letters'
+            formLastName.parentElement.insertBefore(lastNameP, formLastName.nextElementSibling)
         } else {
-            formLastName.classList.add('border-green')
+            formLastName.classList.add('valid')
         }
     }
 
     formLastName.onfocus = function () {
-        formLastName.classList.remove('border-red');
-        lastNameP.removeChild(p);
+        removeP(formLastName);
     }
 
     /* ID VALIDATION */
     var formId = inputs[2];
-    var idP = inputsP[2];
+    var idP = document.createElement('p');
 
     formId.onblur = function () {
         var numbers;
@@ -99,49 +128,47 @@ window.onload = function(){
         }
 
         if (formId.value == ''){
-            formId.classList.add('border-red')
-            p.innerHTML = 'ID is required'
-            idP.appendChild(p);
+            formId.classList.add('error')
+            idP.innerHTML = 'ID is required'
+            formId.parentElement.insertBefore(idP, formId.nextElementSibling)
         } else if (formId.value.length < 7) {
-            formId.classList.add('border-red');
-            p.innerHTML = 'ID should have at least 7 numbers'
-            idP.appendChild(p)
+            formId.classList.add('error');
+            idP.innerHTML = 'ID should have at least 7 numbers'
+            formId.parentElement.insertBefore(idP, formId.nextElementSibling)
         } else if (!numbers){
-            formId.classList.add('border-red')
-            p.innerHTML = 'ID should have only numbers'
-            idP.appendChild(p);
+            formId.classList.add('error')
+            idP.innerHTML = 'ID should have only numbers'
+            formId.parentElement.insertBefore(idP, formId.nextElementSibling)
         } else {
-            formId.classList.add('border-green')
+            formId.classList.add('valid')
         }
     }
 
     formId.onfocus = function () {
-        formId.classList.remove('border-red');
-        idP.removeChild(p);
+        removeP(formId);
     }
 
     /* BIRTHDAY VALIDATION */
     var formBirthday = inputs[3];
-    var birthdayP = inputsP[3];
+    var birthdayP = document.createElement('p');
 
     formBirthday.onblur = function() {
         if (formBirthday.value == '') {
-            formBirthday.classList.add('border-red');
-            p.innerHTML = 'Please enter your birthday'
-            birthdayP.appendChild(p)
+            formBirthday.classList.add('error');
+            birthdayP.innerHTML = 'Birthday is required'
+            formBirthday.parentElement.insertBefore(birthdayP, formBirthday.nextElementSibling)
         } else {
-            formBirthday.classList.add('border-green')
+            formBirthday.classList.add('valid')
         }
+    }
 
-        formBirthday.onfocus = function () {
-            formBirthday.classList.remove('border-red');
-            birthdayP.removeChild(p);
-        }
+    formBirthday.onfocus = function () {
+        removeP(formBirthday);
     }
 
     /* PHONE VALIDATION */
     var formPhone = inputs[4];
-    var phoneP = inputsP[4];
+    var phoneP = document.createElement('p');
 
     formPhone.onblur = function () {
         var numbers;
@@ -157,45 +184,254 @@ window.onload = function(){
         }
 
         if (formPhone.value == ''){
-            formPhone.classList.add('border-red')
-            p.innerHTML = 'Phone number is required'
-            phoneP.appendChild(p);
+            formPhone.classList.add('error')
+            phoneP.innerHTML = 'Phone number is required'
+            formPhone.parentElement.insertBefore(phoneP, formPhone.nextElementSibling)
         } else if (!numbers){
-            formPhone.classList.add('border-red')
-            p.innerHTML = 'Phone number should have only numbers'
-            phoneP.appendChild(p);
+            formPhone.classList.add('error')
+            phoneP.innerHTML = 'Phone number should have only numbers'
+            formPhone.parentElement.insertBefore(phoneP, formPhone.nextElementSibling)
         } else if (formPhone.value.length != 10) {
-            formPhone.classList.add('border-red');
-            p.innerHTML = 'Phone number should have 10 numbers'
-            phoneP.appendChild(p)
+            formPhone.classList.add('error');
+            phoneP.innerHTML = 'Phone number should have 10 numbers'
+            formPhone.parentElement.insertBefore(phoneP, formPhone.nextElementSibling)
         } else {
-            formPhone.classList.add('border-green')
+            formPhone.classList.add('valid')
         }
     }
 
     formPhone.onfocus = function () {
-        formPhone.classList.remove('border-red');
-        phoneP.removeChild(p);
+        removeP(formPhone);
     }
 
-    /* ADDRESS VALIDATION */
-    var formAddress = inputs[5];
-    var addressP = inputsP[5];
+    /* ADRESS VALIDATION */
+    var formAdress = inputs[5];
+    var adressP = document.createElement('p');
 
-    formAddress.onblur = function() {
-        if ((formAddress.value).length == '') {
-            formAddress.classList.add('border-red')
-            p.innerHTML = ('Address is required')
-            addressP.appendChild(p)
+    formAdress.onblur = function() {
+        var blankSpace = formAdress.value.indexOf(' ')
+        if ((formAdress.value).length == '') {
+            formAdress.classList.add('error')
+            adressP.innerHTML = ('Adress is required')
+            formAdress.parentElement.insertBefore(adressP, formAdress.nextElementSibling)
+        } else if ((formAdress.value).length < 5) {
+            formAdress.classList.add('error')
+            adressP.innerHTML = ('Adress must have at least 5 characters')
+            formAdress.parentElement.insertBefore(adressP, formAdress.nextElementSibling)
+        } else if (!hasLetters((formAdress.value).substring(0, blankSpace)) || !hasNumbers((formAdress.value).substring(blankSpace+1, (formAdress.value).length))){
+            formAdress.classList.add('error')
+            adressP.innerHTML = ('Wrong adress format')
+            formAdress.parentElement.insertBefore(adressP, formAdress.nextElementSibling)
         } else {
-            formAddress.classList.add('border-green')
-
+            formAdress.classList.add('valid')
         }
     }
 
     formAdress.onfocus = function() {
-        formAddress.classList.remove('border-red');
-        addressP.removeChild(p);
+        removeP(formAdress);
     }
 
+    /* CITY VALIDATION */
+    var formCity = inputs[6];
+    var cityP = document.createElement('p');
+
+    formCity.onblur = function() {
+        var validationLetters = false
+        var validationNumber = false
+
+        for(i=0; i<formCity.value.length; i++){
+            if (numbers.indexOf(formCity.value.charAt(i),0)!=-1){
+                validationNumber = true;
+            }
+            if (letters.indexOf(formCity.value.charAt(i),0)!=-1){
+                validationLetters = true;
+            }
+        }
+
+        if (formCity.value === '') {
+            formCity.classList.add('error');
+            cityP.innerHTML = 'City name is required';
+            formCity.parentElement.insertBefore(cityP, formCity.nextElementSibling)
+        } else if (formCity.value.length < 4 || validationNumber != true && validationLetters != true) {
+            formCity.classList.add('error');
+            cityP.innerHTML = 'City name must have more than 3 characters of letters&numbers';
+            formCity.parentElement.insertBefore(cityP, formCity.nextElementSibling)
+        } else {
+            formCity.classList.add('valid');
+        }
+    }
+
+    formCity.onfocus = function () {
+        removeP(formCity);
+    }
+
+    /*  ZIP CODE VALIDATION */
+    var formZipCode = inputs[7];
+    var zipCodeP = document.createElement('p');
+
+    formZipCode.onblur = function () {
+        var numbers;
+        for (var i = 0; i < formZipCode.value.length; i++){
+            var element = formZipCode.value;
+            var charCode = element.charCodeAt(i)
+            if ((charCode > 47 && charCode < 58)) {
+                numbers = true;
+            } else {
+                numbers = false;
+                break
+            }
+        }
+
+        if (formZipCode.value == ''){
+            formZipCode.classList.add('error')
+            zipCodeP.innerHTML = 'Zip code is required'
+            formZipCode.parentElement.insertBefore(zipCodeP, formZipCode.nextElementSibling)
+        } else if (!numbers){
+            formZipCode.classList.add('error')
+            zipCodeP.innerHTML = 'Zip code should have only numbers'
+            formZipCode.parentElement.insertBefore(zipCodeP, formZipCode.nextElementSibling)
+        } else if (formZipCode.value.length < 4 || formZipCode.value.length > 5) {
+            formZipCode.classList.add('error');
+            zipCodeP.innerHTML = 'Zip code should have 4 or 5 numbers'
+            formZipCode.parentElement.insertBefore(zipCodeP, formZipCode.nextElementSibling)
+        } else {
+            formZipCode.classList.add('valid')
+        }
+    }
+
+    formZipCode.onfocus = function () {
+        removeP(formZipCode);
+    }
+
+
+    /* EMAIL VALIDATION */
+    var formEmail = inputs[8];
+    var emailP = document.createElement('p');
+    var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+
+    formEmail.onblur = function() {
+        if (formEmail.value === '') {
+            formEmail.classList.add('error');
+            emailP.innerHTML = 'Email is required'
+            formEmail.parentElement.insertBefore(emailP, formEmail.nextElementSibling)
+        } else if (!emailExpression.test(formEmail.value)) {
+            formEmail.classList.add('error');
+            emailP.innerHTML = 'Correct format should be email@domain.com';
+            formEmail.parentElement.insertBefore(emailP, formEmail.nextElementSibling)
+        } else {
+            formEmail.classList.add('valid');
+        }
+    }
+
+    formEmail.onfocus = function () {
+        removeP(formEmail);
+    }
+
+    /* PASSWORD VALIDATION */
+    var formPassword = inputs[9];
+    var passwordP = document.createElement('p');
+
+    formPassword.onblur = function() {
+
+        var validationLetters = false
+        var validationNumber = false
+
+        for(i=0; i<formPassword.value.length; i++){
+            if (numbers.indexOf(formPassword.value.charAt(i),0)!=-1){
+                validationNumber = true;
+            }
+            if (letters.indexOf(formPassword.value.charAt(i),0)!=-1){
+                validationLetters = true;
+            }
+        }
+
+        if (formPassword.value === '') {
+            formPassword.classList.add('error');
+            passwordP.innerHTML = 'Password is required';
+            formPassword.parentElement.insertBefore(passwordP, formPassword.nextElementSibling)
+        } else if (formPassword.value.length < 8 || validationNumber != true || validationLetters != true) {
+            formPassword.classList.add('error');
+            passwordP.innerHTML = 'Password must have at least 8 characters of letters and numbers';
+            formPassword.parentElement.insertBefore(passwordP, formPassword.nextElementSibling)
+        } else {
+            formPassword.classList.add('valid');
+        }
+    }
+
+    formPassword.onfocus = function () {
+        removeP(formPassword);
+    }
+
+    /* PASSWORD REPEAT VALIDATION */
+    var formPassword2 = inputs[10];
+    var password2P = document.createElement('p');
+
+    formPassword2.onblur = function() {
+        var validationLetters = false
+        var validationNumber = false
+
+        for(i=0; i<formPassword2.value.length; i++){
+            if (numbers.indexOf(formPassword2.value.charAt(i),0)!=-1){
+                validationNumber = true;
+            }
+            if (letters.indexOf(formPassword2.value.charAt(i),0)!=-1){
+                validationLetters = true;
+            }
+        }
+
+        if (formPassword2.value === '') {
+            formPassword2.classList.add('error');
+            password2P.innerHTML = 'Password is required';
+            formPassword2.parentElement.insertBefore(password2P, formPassword2.nextElementSibling)
+        } else if (formPassword2.value.length < 8 || validationNumber != true || validationLetters != true) {
+            formPassword2.classList.add('error');
+            password2P.innerHTML = 'Password must have at least 8 characters of letters and numbers';
+            formPassword2.parentElement.insertBefore(password2P, formPassword2.nextElementSibling)
+        } else if (formPassword.value != formPassword2.value){
+            formPassword2.classList.add('error');
+            password2P.innerHTML = "Passwords don't match";
+            formPassword2.parentElement.insertBefore(password2P, formPassword2.nextElementSibling)
+        } else {
+            formPassword2.classList.add('valid');
+        }
+    }
+
+    formPassword2.onfocus = function () {
+        removeP(formPassword2);
+    }
+
+    /* BUTTON */
+    var button = document.getElementById('btn-signup');
+
+    button.onclick = function (e) {
+        e.preventDefault();
+        var completed = 0;
+        var errors = [];
+        var message = '';
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].classList.contains('valid')) {
+                completed++;
+            } else if (inputs[i].classList.contains('error')) {
+                errors.push(inputs[i].nextElementSibling);
+            }
+        }
+        if (completed == inputs.length) {
+            message = 'Sign up successful!\n';
+            for (var i = 0; i < inputs.length; i++) {
+                message += inputs[i].name + ': ' + inputs[i].value + '\n';
+            }
+
+        } else if (errors.length == 0) {
+            message = 'All fields are required.';
+        } else {
+            message = 'Something went wrong\n';
+            if (errors.length + completed !== inputs.length) {
+                message += 'Complete all fields\n'
+            }
+            for (var i = 0; i < errors.length; i++) {
+                message += errors[i].innerHTML + '\n';
+            }
+        }
+        alert(message);
+    }
 }
