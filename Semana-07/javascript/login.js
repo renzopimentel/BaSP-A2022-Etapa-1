5b101b97ab6c5e91259dc299d523efea74b261c9
Renzo Pimentel Uzal <renzopimentel@gmail.com>
2022-10-03 23:49:11 -0300

    BaSP-W07-2022: finished login form with fetch and modal

--- Semana-07/javascript/login.js
+++ Semana-07/javascript/login.js
@@ -1,39 +1,55 @@
 window.onload = function() {
-
-    var p = document.createElement('p');
+    var emailInput = document.getElementById('email-input');
+    var passwordInput = document.getElementById('password-input');
+    var emailP = document.createElement('p');
+    var passP = document.createElement('p');
     var button = document.getElementById('btn-login');
+    var modalContainer = document.getElementById("modal-suscription");
+    var modalTitle = document.querySelector(".modal-content > h3");
+    var modalData = document.querySelector(".modal-content > ul");
+    var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+    var formEmail = document.getElementById('email-form');
+    var formPassword = document.getElementById('password-form');
+    var numbers = "0123456789";
+    var letters = "abcdefghyjklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    var closeBtn = document.getElementsByClassName("close")[0];
 
-   /* EMAIL VALIDATION */
-   var formEmail = document.getElementById('email-form');
-   var emailInput = document.getElementById('email-input');
-   var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+    function removeP(input) {
+        input.classList.remove('red-border');
+        if (input.nextElementSibling) {
+            input.parentElement.removeChild(input.nextElementSibling);
+        }
+    }
+
+    closeBtn.onclick = function() {
+        modalContainer.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modalContainer) {
+          modalContainer.style.display = "none";
+        }
+     }
 
     formEmail.onblur = function() {
         if (formEmail.value === '') {
             formEmail.classList.add('error');
-            p.innerHTML = 'Email is required'
-            emailInput.appendChild(p);
+            emailP.innerHTML = 'Email is required'
+            formEmail.parentElement.insertBefore(emailP, formEmail.nextElementSibling)
         } else if (!emailExpression.test(formEmail.value)) {
             formEmail.classList.add('error');
-            p.innerHTML = 'Correct format should be email@domain.com';
-            emailInput.appendChild(p);
+            emailP.innerHTML = 'Correct format should be email@domain.com';
+            formEmail.parentElement.insertBefore(emailP, formEmail.nextElementSibling)
         } else {
             formEmail.classList.add('valid');
         }
     }
 
     formEmail.onfocus = function () {
-        formEmail.classList.remove('error');
-        emailInput.removeChild(p);
+        removeP(formEmail);
     }
 
-    /* PASSWORD VALIDATION */
-    var formPassword = document.getElementById('password-form');
-    var passwordInput = document.getElementById('password-input');
-
     formPassword.onblur = function() {
-        var numbers = "0123456789";
-        var letters = "abcdefghyjklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
         var validationNumber = false
         var validationLetters = false
 
@@ -48,28 +64,58 @@ window.onload = function() {
 
         if (formPassword.value === '') {
             formPassword.classList.add('error');
-            p.innerHTML = 'Password is required';
-            passwordInput.appendChild(p);
+            passP.innerHTML = 'Password is required';
+            formPassword.parentElement.insertBefore(passP, formPassword.nextElementSibling)
         } else if (formPassword.value.length < 8 || validationNumber != true || validationLetters != true) {
             formPassword.classList.add('error');
-            p.innerHTML = 'Password must have at least 8 characters of letters and numbers';
-            passwordInput.appendChild(p);
+            passP.innerHTML = 'Password must have at least 8 characters of letters and numbers';
+            formPassword.parentElement.insertBefore(passP, formPassword.nextElementSibling)
         } else {
             formPassword.classList.add('valid');
         }
     }
 
     formPassword.onfocus = function () {
-        formPassword.classList.remove('error');
-        passwordInput.removeChild(p);
+        removeP(formPassword);
     }
 
-    /* BUTTON */
-    button.onclick = function(){
-        if (formEmail.classList.contains('valid') && formPassword.classList.contains('valid')){
-            alert('Mail: ' + formEmail.value + '\nPassword: ' + formPassword.value);
-        } else {
-            alert('Login Error');
-        }
+    function successfulModal(data){
+        modalContainer.style.display = " block ";
+        modalTitle.innerHTML = " Login Succesfully! ";
+        modalData.innerHTML = `${data.msg}`;
+    }
+
+    function errorModal(errorInfo){
+        var jsonString = JSON.stringify(errorInfo);
+        modalContainer.style.display = "block";
+        modalTitle.innerHTML = " Sorry, there was an error ";
+        modalData.innerHTML = `${errorInfo.msg}`;
+    }
+
+    button.onclick = function(e){
+        var url = 'https://basp-m2022-api-rest-server.herokuapp.com/login?email='+formEmail.value+'&password='+formPassword.value;
+        fetch(url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                if(data.success){
+                    // alert('Login successful! ' + data.msg + '\nEmail: ' + formEmail.value + '\nPassword: ' + formPassword.value) ;
+                    successfulModal(data);
+                    console.log(data);
+                } else if(data.errors){
+                    var messages = ''
+                    for (var error of data.errors) {
+                        messages += error.msg + '\n'
+                    }
+                    throw new Error(messages);
+                } else {
+                    errorModal(data);
+                }
+            })
+            .catch(function(error) {
+                console.log(error)
+            })
+        e.preventDefault();
   }
 }
